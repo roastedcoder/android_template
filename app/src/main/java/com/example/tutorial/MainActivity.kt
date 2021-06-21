830900e00ed03a23b76c05da3c5ac586c8d43fc8
Suraj Kumar <vermasuraj412@gmail.com>
2021-06-21 16:46:32 +0530

    broadcast receiver (Airplane Mode Changed Receiver)

--- app/src/main/java/com/example/tutorial/MainActivity.kt
+++ app/src/main/java/com/example/tutorial/MainActivity.kt
@@ -1,10 +1,7 @@
 package com.example.tutorial
 
 import android.app.*
-import android.content.ClipData
-import android.content.ClipDescription
-import android.content.Context
-import android.content.Intent
+import android.content.*
 import android.graphics.Color
 import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
@@ -34,69 +31,21 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var receiver: AirplaneModeChangedReceiver
 
-    @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        llTop.setOnDragListener(dragListener)
-        llBottom.setOnDragListener(dragListener)
-
-
-        dragView.setOnLongClickListener {
-            val clipText = "Dropped"
-            val item = ClipData.Item(clipText)
-            val mimeTypes = arrayOf(ClipDescription.MIMETYPE_TEXT_PLAIN)
-            val data = ClipData(clipText, mimeTypes, item)
-
-            val dragShadowBuilder = View.DragShadowBuilder(it)
-
-            it.startDragAndDrop(data, dragShadowBuilder, it, 0)
-
-            it.visibility = View.INVISIBLE
-            true
-
+        receiver = AirplaneModeChangedReceiver()
+        IntentFilter(Intent.ACTION_AIRPLANE_MODE_CHANGED).also {
+            registerReceiver(receiver, it)
         }
     }
 
-    private val dragListener = View.OnDragListener { view, dragEvent ->
-        when(dragEvent.action) {
-            DragEvent.ACTION_DRAG_STARTED -> {
-                dragEvent.clipDescription.hasMimeType(ClipDescription.MIMETYPE_TEXT_PLAIN)
-            }
-            DragEvent.ACTION_DRAG_ENTERED -> {
-                view.invalidate()
-                true
-            }
-            DragEvent.ACTION_DRAG_LOCATION -> true
+    override fun onStop() {
+        super.onStop()
 
-            DragEvent.ACTION_DRAG_EXITED -> {
-                view.invalidate()
-                true
-            }
-
-            DragEvent.ACTION_DROP -> {
-                val item = dragEvent.clipData.getItemAt(0)
-                val dragData = item.text
-                Toast.makeText(this, dragData, Toast.LENGTH_SHORT).show()
-
-                view.invalidate()
-                val v = dragEvent.localState as View
-                val owner = v.parent as ViewGroup
-                owner.removeView(v)
-
-                val destination = view as LinearLayout
-                destination.addView(v)
-                v.visibility = View.VISIBLE
-
-                true
-            }
-            DragEvent.ACTION_DRAG_ENDED -> {
-                view.invalidate()
-                true
-            }
-            else -> false
-        }
+        unregisterReceiver(receiver)
     }
 }
