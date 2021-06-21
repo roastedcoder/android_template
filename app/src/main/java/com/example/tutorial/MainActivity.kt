6ad9ed3983dde0e88b6b031f2824ad4c047ea380
Suraj Kumar <vermasuraj412@gmail.com>
2021-06-21 16:22:58 +0530

    drag n drop

--- app/src/main/java/com/example/tutorial/MainActivity.kt
+++ app/src/main/java/com/example/tutorial/MainActivity.kt
@@ -1,18 +1,29 @@
 package com.example.tutorial
 
-import android.app.Activity
+import android.app.*
+import android.content.ClipData
+import android.content.ClipDescription
 import android.content.Context
 import android.content.Intent
+import android.graphics.Color
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.DragEvent
+import android.view.DragEvent.ACTION_DRAG_LOCATION
 import android.view.MenuItem
 import android.view.View
+import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
+import android.widget.LinearLayout
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AlertDialog
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.viewpager.widget.ViewPager
@@ -24,43 +35,68 @@ import kotlinx.android.synthetic.main.activity_main.*
 class MainActivity : AppCompatActivity() {
 
 
+    @RequiresApi(Build.VERSION_CODES.N)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // we should use shared preferences to save small data such as setting preferences, game high-score etc.
-        // for larger data(to do app) we need a database
-        val sharedPref = getSharedPreferences("myPref", Context.MODE_PRIVATE)
+        llTop.setOnDragListener(dragListener)
+        llBottom.setOnDragListener(dragListener)
 
-        // whenever we want to write to shared preferences we need an editor
-        val editor = sharedPref.edit()
 
-        btnSave.setOnClickListener {
-            val name = etName.text.toString()
-            val age = etAge.text.toString().toInt()
-            val isAdult = cbAdult.isChecked
+        dragView.setOnLongClickListener {
+            val clipText = "Dropped"
+            val item = ClipData.Item(clipText)
+            val mimeTypes = arrayOf(ClipDescription.MIMETYPE_TEXT_PLAIN)
+            val data = ClipData(clipText, mimeTypes, item)
 
-            editor.apply {
-                // it takes a key- value pair to fetch the data later
-                putString("name", name)
-                putInt("age", age)
-                putBoolean("isAdult", isAdult)
-                apply()
-                // we can also use commit here instead of apply
-                // but since it puts the data synchronously in shared preferences
-                // and blocks the main thread and UI
-                // hence we prefer to use apply here
-            }
+            val dragShadowBuilder = View.DragShadowBuilder(it)
+
+            it.startDragAndDrop(data, dragShadowBuilder, it, 0)
+
+            it.visibility = View.INVISIBLE
+            true
+
         }
+    }
 
-        btnLoad.setOnClickListener {
-            val name = sharedPref.getString("name", null)
-            val age = sharedPref.getInt("age", 0)
-            val isAdult = sharedPref.getBoolean("isAdult", false)
+    private val dragListener = View.OnDragListener { view, dragEvent ->
+        when(dragEvent.action) {
+            DragEvent.ACTION_DRAG_STARTED -> {
+                dragEvent.clipDescription.hasMimeType(ClipDescription.MIMETYPE_TEXT_PLAIN)
+            }
+            DragEvent.ACTION_DRAG_ENTERED -> {
+                view.invalidate()
+                true
+            }
+            DragEvent.ACTION_DRAG_LOCATION -> true
 
-            etName.setText(name.toString())
-            etAge.setText(age.toString())
-            cbAdult.isChecked = isAdult
+            DragEvent.ACTION_DRAG_EXITED -> {
+                view.invalidate()
+                true
+            }
+
+            DragEvent.ACTION_DROP -> {
+                val item = dragEvent.clipData.getItemAt(0)
+                val dragData = item.text
+                Toast.makeText(this, dragData, Toast.LENGTH_SHORT).show()
+
+                view.invalidate()
+                val v = dragEvent.localState as View
+                val owner = v.parent as ViewGroup
+                owner.removeView(v)
+
+                val destination = view as LinearLayout
+                destination.addView(v)
+                v.visibility = View.VISIBLE
+
+                true
+            }
+            DragEvent.ACTION_DRAG_ENDED -> {
+                view.invalidate()
+                true
+            }
+            else -> false
         }
     }
 }
