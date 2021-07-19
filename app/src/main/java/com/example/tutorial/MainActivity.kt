3f15946849f4598c25e8a3b0bbe0470a00898dd7
Suraj Kumar <vermasuraj412@gmail.com>
2021-07-19 19:12:34 +0530

    DataBinding

--- app/src/main/java/com/example/tutorial/MainActivity.kt
+++ app/src/main/java/com/example/tutorial/MainActivity.kt
@@ -3,27 +3,20 @@ package com.example.tutorial
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
+import com.example.tutorial.databinding.ActivityMainBinding
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        val mainViewModel = ViewModelProvider(this).get(MainViewModel::class.java)
+        val quoteObj = Quote("With great power comes great responsibilities", "Peter Parker")
+        binding.quote = quoteObj
 
-        mainViewModel.value.observe(this, {
-            tvValue.text = it
-        })
-
-        btnUpdate.setOnClickListener {
-            mainViewModel.updateLiveData()
-        }
-
-        btnReset.setOnClickListener {
-            mainViewModel.resetLiveData()
-        }
     }
 }
