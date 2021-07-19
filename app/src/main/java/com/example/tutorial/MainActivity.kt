a68353f6863898b301105e0dc86f204fbf5d808c
Suraj Kumar <vermasuraj412@gmail.com>
2021-07-19 14:37:26 +0530

    LiveData

--- app/src/main/java/com/example/tutorial/MainActivity.kt
+++ app/src/main/java/com/example/tutorial/MainActivity.kt
@@ -2,48 +2,28 @@ package com.example.tutorial
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import androidx.lifecycle.Observer
+import android.util.Log
 import androidx.lifecycle.ViewModelProvider
-import androidx.recyclerview.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.android.synthetic.main.item_task.*
 
-class MainActivity : AppCompatActivity(), TaskClickListener {
-
-    lateinit var viewModel: TaskViewModel
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        rvTasks.layoutManager = LinearLayoutManager(this)
-        val adapter = TaskAdapter(this, this)
-        rvTasks.adapter = adapter
+        val mainViewModel = ViewModelProvider(this).get(MainViewModel::class.java)
 
-        viewModel = ViewModelProvider(
-            this,
-            ViewModelProvider.AndroidViewModelFactory.getInstance(application)
-        ).get(TaskViewModel::class.java)
-
-        viewModel.allTasks.observe(this, Observer { list ->
-            list?.let {
-                adapter.updateList(it)
-            }
+        mainViewModel.value.observe(this, {
+            tvValue.text = it
         })
-    }
 
-    override fun onItemClicked(tasks: Tasks) {
-        viewModel.deleteTask(tasks)
-    }
+        btnUpdate.setOnClickListener {
+            mainViewModel.updateLiveData()
+        }
 
-    fun addTask(view: View) {
-        val text = etInput.text.toString()
-        text.trim()
-        if (text.isNotEmpty()) {
-            val newTask = Tasks(text)
-            viewModel.addTask(newTask)
-            etInput.text.clear()
+        btnReset.setOnClickListener {
+            mainViewModel.resetLiveData()
         }
     }
 }
