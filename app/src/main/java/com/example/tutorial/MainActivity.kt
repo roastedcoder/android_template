2e252946cddd46e66edc55e50e7639e57f5536ae
Suraj Kumar <vermasuraj412@gmail.com>
2021-08-03 00:15:29 +0530

    layout for firebase auth

--- app/src/main/java/com/example/tutorial/MainActivity.kt
+++ app/src/main/java/com/example/tutorial/MainActivity.kt
@@ -2,37 +2,15 @@ package com.example.tutorial
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import android.view.View
-import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.ViewModelProvider
-import androidx.room.Room
-import com.example.tutorial.databinding.ActivityMainBinding
-import kotlinx.android.synthetic.main.activity_main.*
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import java.util.*
+import com.google.firebase.auth.FirebaseAuth
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var binding: ActivityMainBinding
-    lateinit var mainViewModel: MainViewModel
+    lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        setContentView(R.layout.activity_main)
 
-        val dao = QuoteDB.getDatabase(applicationContext).quoteDao()
-        val repository = QuoteRepository(dao)
-        mainViewModel = ViewModelProvider(this, MainViewModelFactory(repository)).get(MainViewModel::class.java)
-
-        mainViewModel.getQuotes().observe(this, androidx.lifecycle.Observer {
-            binding.quotes = it.toString()
-        })
-
-        binding.btnInsert.setOnClickListener {
-            val quote= Quote(0, "this is test", "testing")
-            mainViewModel.insertQuote(quote)
-        }
 
     }
 }
