5f6b3ddefa1f2aa04e609eb7dac4be9ec231fb6a
Suraj Kumar <vermasuraj412@gmail.com>
2021-08-05 22:28:42 +0530

    firebase authentication

--- app/src/main/java/com/example/tutorial/MainActivity.kt
+++ app/src/main/java/com/example/tutorial/MainActivity.kt
@@ -2,7 +2,15 @@ package com.example.tutorial
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
+import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
 
@@ -11,6 +19,73 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        auth = FirebaseAuth.getInstance()
+        checkLoggedInState()
 
+        btnRegister.setOnClickListener {
+            registerUser()
+        }
+
+        btnLogin.setOnClickListener {
+            loginUser()
+        }
+
+        btnLogout.setOnClickListener {
+            auth.signOut()
+            checkLoggedInState()
+        }
+    }
+
+    private fun registerUser() {
+        val email = etEmailRegister.text.toString()
+        etEmailRegister.text.clear()
+        val password = etPasswordRegister.text.toString()
+        etPasswordRegister.text.clear()
+        if(email.isNotEmpty() && password.isNotEmpty()) {
+            CoroutineScope(Dispatchers.IO).launch {
+                try {
+                    auth.createUserWithEmailAndPassword(email, password).await()
+                    withContext(Dispatchers.Main) {
+                        checkLoggedInState()
+                    }
+                }
+                catch(e: Exception) {
+                    withContext(Dispatchers.Main) {
+                        Toast.makeText(this@MainActivity, e.message, Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
+    }
+
+    private fun loginUser() {
+        val email = etEmailLogin.text.toString()
+        etEmailLogin.text.clear()
+        val password = etPasswordLogin.text.toString()
+        etPasswordLogin.text.clear()
+        if(email.isNotEmpty() && password.isNotEmpty()) {
+            CoroutineScope(Dispatchers.IO).launch {
+                try {
+                    auth.signInWithEmailAndPassword(email, password).await()
+                    withContext(Dispatchers.Main) {
+                        checkLoggedInState()
+                    }
+                }
+                catch(e: Exception) {
+                    withContext(Dispatchers.Main) {
+                        Toast.makeText(this@MainActivity, e.message, Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        }
+    }
+
+    private fun checkLoggedInState() {
+        if(auth.currentUser == null) {
+            tvLoginState.text = "You are not logged in!"
+        }
+        else {
+            tvLoginState.text = "You are logged in!"
+        }
     }
 }
