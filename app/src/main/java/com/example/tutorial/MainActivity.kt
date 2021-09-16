08b7d9ff3eb2274f09d95c010113aa43516a702f
Suraj Kumar <vermasuraj412@gmail.com>
2021-09-16 22:11:30 +0530

    startActivityForResult deprecation solution

--- app/src/main/java/com/example/tutorial/MainActivity.kt
+++ app/src/main/java/com/example/tutorial/MainActivity.kt
@@ -2,7 +2,10 @@ package com.example.tutorial
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.Toast
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.contract.ActivityResultContracts
 import com.google.firebase.auth.FirebaseAuth
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.coroutines.CoroutineScope
@@ -14,78 +17,18 @@ import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        auth = FirebaseAuth.getInstance()
-        checkLoggedInState()
-
-        btnRegister.setOnClickListener {
-            registerUser()
-        }
-
-        btnLogin.setOnClickListener {
-            loginUser()
-        }
-
-        btnLogout.setOnClickListener {
-            auth.signOut()
-            checkLoggedInState()
-        }
-    }
-
-    private fun registerUser() {
-        val email = etEmailRegister.text.toString()
-        etEmailRegister.text.clear()
-        val password = etPasswordRegister.text.toString()
-        etPasswordRegister.text.clear()
-        if(email.isNotEmpty() && password.isNotEmpty()) {
-            CoroutineScope(Dispatchers.IO).launch {
-                try {
-                    auth.createUserWithEmailAndPassword(email, password).await()
-                    withContext(Dispatchers.Main) {
-                        checkLoggedInState()
-                    }
-                }
-                catch(e: Exception) {
-                    withContext(Dispatchers.Main) {
-                        Toast.makeText(this@MainActivity, e.message, Toast.LENGTH_SHORT).show()
-                    }
-                }
+        val getImage = registerForActivityResult(
+            ActivityResultContracts.GetContent(),
+            ActivityResultCallback {
+                ivImage.setImageURI(it)
             }
-        }
-    }
-
-    private fun loginUser() {
-        val email = etEmailLogin.text.toString()
-        etEmailLogin.text.clear()
-        val password = etPasswordLogin.text.toString()
-        etPasswordLogin.text.clear()
-        if(email.isNotEmpty() && password.isNotEmpty()) {
-            CoroutineScope(Dispatchers.IO).launch {
-                try {
-                    auth.signInWithEmailAndPassword(email, password).await()
-                    withContext(Dispatchers.Main) {
-                        checkLoggedInState()
-                    }
-                }
-                catch(e: Exception) {
-                    withContext(Dispatchers.Main) {
-                        Toast.makeText(this@MainActivity, e.message, Toast.LENGTH_SHORT).show()
-                    }
-                }
-            }
-        }
-    }
-
-    private fun checkLoggedInState() {
-        if(auth.currentUser == null) {
-            tvLoginState.text = "You are not logged in!"
-        }
-        else {
-            tvLoginState.text = "You are logged in!"
+        )
+        btnChoose.setOnClickListener {
+            getImage.launch("image/*")
         }
     }
 }
